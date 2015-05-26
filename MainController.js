49f5aa493bdd91b5a435e2ff2706adecd90c4802
charly3p <charlyfb90@gmail.com>
2015-05-26 23:08:21 +0200

    Refactoring classes and code

--- MainController.js
+++
@@ -1,6 +0,0 @@
-angular
-	.module('paracordistry')
-	.controller('MainController', function() {
-	    var vm = this;
-	    vm.message = 'Hola, Mundo del Paracord!';
-	});
--- _module.js
+++
@@ -1,3 +0,0 @@
-angular
-    .module('paracordistry',
-        ['paracordistry.shop','paracordistry.desvan','paracordistry.about','ngRoute']);
